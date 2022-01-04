5e137748989fff923ebf2629b62f29ce893da6bd
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-01-04 07:31:54 -0600

    added a dark light toggle

--- app/index.js
+++ app/index.js
@@ -10,3 +10,44 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// Toggle theme switcher
+const header = document.querySelector('.header');
+const btn = document.querySelector('.slider');
+const body = document.querySelector('body');
+const projectsSec = document.querySelector('.projects-section');
+const projects = document.querySelectorAll('.project');
+const buttons = document.querySelectorAll('.button');
+const aboutSec = document.querySelector('.about-section');
+const headings = document.querySelectorAll('.em1');
+const contactSec = document.querySelector('.contact-section');
+const fas = document.querySelectorAll('.fab');
+const fai = document.querySelector('.fas');
+const smalls = document.querySelectorAll('small');
+
+btn.addEventListener('click', function() {
+    body.classList.toggle('dark-theme');
+    header.classList.toggle('dark-theme');
+    projectsSec.classList.toggle('dark-theme');
+
+    for (let project of projects) {
+        project.classList.toggle('dark-theme');
+    }
+    for (let button of buttons) {
+        button.classList.toggle('dark-theme');
+    }
+    for (let heading of headings) {
+        heading.classList.toggle('dark-theme');
+    }
+    for (let fa of fas) {
+        fa.classList.toggle('dark-theme');
+    }
+    for (let small of smalls) {
+        small.classList.toggle('dark-theme');
+    }
+
+    fai.classList.toggle('dark-theme');
+    aboutSec.classList.toggle('dark-theme');
+    contactSec.classList.toggle('dark-theme');
+    // h3dark.classList.toggle('dark-theme');
+})
